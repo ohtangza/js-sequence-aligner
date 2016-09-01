b2a5ebd5f2a6e45f14170ca4867968ca6e0ba8b2
JeungminOh <tangza@gmail.com>
2016-09-01 20:34:59 +0900

    Bug fix on algorithm (it does not reach to the end)

--- needleman-wunsch.js
+++ needleman-wunsch.js
@@ -1,25 +1,72 @@
 var stem = require('stem-porter');
 
+var stopWords = "a,able,about,above,abst,accordance,according,accordingly,across,act,actually,added,adj,\
+affected,affecting,affects,after,afterwards,again,against,ah,all,almost,alone,along,already,also,although,\
+always,am,among,amongst,an,and,announce,another,any,anybody,anyhow,anymore,anyone,anything,anyway,anyways,\
+anywhere,apparently,approximately,are,aren,arent,arise,around,as,aside,ask,asking,at,auth,available,away,awfully,\
+b,back,be,became,because,become,becomes,becoming,been,before,beforehand,begin,beginning,beginnings,begins,behind,\
+being,believe,below,beside,besides,between,beyond,biol,both,brief,briefly,but,by,c,ca,came,can,cannot,can't,cause,causes,\
+certain,certainly,co,com,come,comes,contain,containing,contains,could,couldnt,d,date,did,didn't,different,do,does,doesn't,\
+doing,done,don't,down,downwards,due,during,e,each,ed,edu,effect,eg,eight,eighty,either,else,elsewhere,end,ending,enough,\
+especially,et,et-al,etc,even,ever,every,everybody,everyone,everything,everywhere,ex,except,f,far,few,ff,fifth,first,five,fix,\
+followed,following,follows,for,former,formerly,forth,found,four,from,further,furthermore,g,gave,get,gets,getting,give,given,gives,\
+giving,go,goes,gone,got,gotten,h,had,happens,hardly,has,hasn't,have,haven't,having,he,hed,hence,her,here,hereafter,hereby,herein,\
+heres,hereupon,hers,herself,hes,hi,hid,him,himself,his,hither,home,how,howbeit,however,hundred,i,id,ie,if,i'll,im,immediate,\
+immediately,importance,important,in,inc,indeed,index,information,instead,into,invention,inward,is,isn't,it,itd,it'll,its,itself,\
+i've,j,just,k,keep,keeps,kept,kg,km,know,known,knows,l,largely,last,lately,later,latter,latterly,least,less,lest,let,lets,like,\
+liked,likely,line,little,'ll,look,looking,looks,ltd,m,made,mainly,make,makes,many,may,maybe,me,mean,means,meantime,meanwhile,\
+merely,mg,might,million,miss,ml,more,moreover,most,mostly,mr,mrs,much,mug,must,my,myself,n,na,name,namely,nay,nd,near,nearly,\
+necessarily,necessary,need,needs,neither,never,nevertheless,new,next,nine,ninety,no,nobody,non,none,nonetheless,noone,nor,\
+normally,nos,not,noted,nothing,now,nowhere,o,obtain,obtained,obviously,of,off,often,oh,ok,okay,old,omitted,on,once,one,ones,\
+only,onto,or,ord,other,others,otherwise,ought,our,ours,ourselves,out,outside,over,overall,owing,own,p,page,pages,part,\
+particular,particularly,past,per,perhaps,placed,please,plus,poorly,possible,possibly,potentially,pp,predominantly,present,\
+previously,primarily,probably,promptly,proud,provides,put,q,que,quickly,quite,qv,r,ran,rather,rd,re,readily,really,recent,\
+recently,ref,refs,regarding,regardless,regards,related,relatively,research,respectively,resulted,resulting,results,right,run,s,\
+said,same,saw,say,saying,says,sec,section,see,seeing,seem,seemed,seeming,seems,seen,self,selves,sent,seven,several,shall,she,shed,\
+she'll,shes,should,shouldn't,show,showed,shown,showns,shows,significant,significantly,similar,similarly,since,six,slightly,so,\
+some,somebody,somehow,someone,somethan,something,sometime,sometimes,somewhat,somewhere,soon,sorry,specifically,specified,specify,\
+specifying,still,stop,strongly,sub,substantially,successfully,such,sufficiently,suggest,sup,sure,t,take,taken,taking,tell,tends,\
+th,than,thank,thanks,thanx,that,that'll,thats,that've,the,their,theirs,them,themselves,then,thence,there,thereafter,thereby,\
+thered,therefore,therein,there'll,thereof,therere,theres,thereto,thereupon,there've,these,they,theyd,they'll,theyre,they've,\
+think,this,those,thou,though,thoughh,thousand,throug,through,throughout,thru,thus,til,tip,to,together,too,took,toward,towards,\
+tried,tries,truly,try,trying,ts,twice,two,u,un,under,unfortunately,unless,unlike,unlikely,until,unto,up,upon,ups,us,use,used,\
+useful,usefully,usefulness,uses,using,usually,v,value,various,'ve,very,via,viz,vol,vols,vs,w,want,wants,was,wasn't,way,we,wed,\
+welcome,we'll,went,were,weren't,we've,what,whatever,what'll,whats,when,whence,whenever,where,whereafter,whereas,whereby,wherein,\
+wheres,whereupon,wherever,whether,which,while,whim,whither,who,whod,whoever,whole,who'll,whom,whomever,whos,whose,why,widely,\
+willing,wish,with,within,without,won't,words,world,would,wouldn't,www,x,y,yes,yet,you,youd,you'll,your,youre,yours,yourself,\
+yourselves,you've,z,zero".split(',');
+
+/*
 var s1 = "GCATGCU";
 var s2 = "GATTACA";
 
-
-s1 = ['I', 'AM', 'HONORED', 'TO', 'BE', 'WITH', 'YOU', 'TODAY', 'AT', 'YOUR', 'GRADUATION'];
-s2 = ['I', 'AM', 'VERY', 'HAPPY', 'TO', 'STAY', 'WITH', 'ME', 'AT', 'YOUR', 'COMMENCEMENT'];
-// 순서가 중요하다!
-
-/*
-s2 = "About a year ago I was diagnosed with cancer. I had a scan at 7:30 in the morning, and it clearly showed a tumor on my pancreas. I didn’t even know what a pancreas was. The doctors told me this was almost certainly a type of cancer that is incurable, and that I should expect to live no longer than three to six months. My doctor advised me to go home and get my affairs in order, which is doctor’s code for prepare to die. It means to try to tell your kids everything you thought you’d have the next 10 years to tell them in just a few months. It means to make sure everything is buttoned up so that it will be as easy as possible for your family. It means to say your goodbyes. I lived with that diagnosis all day. Later that evening I had a biopsy, where they stuck an endoscope down my throat, through my stomach and into my intestines, put a needle into my pancreas and got a few cells from the tumor. I was sedated, but my wife, who was there, told me that when they viewed the cells under a microscope the doctors started crying because it turned out to be a very rare form of pancreatic cancer that is curable with surgery. I had the surgery and I’m fine now.";
-s2 = s2.toLowerCase().replace('.', '').replace(',', '').split(' ');
-//console.log(s1);
-s1 = 'doctor told me that this was almost certainly a type of cancer that is incurable'.split(' ');
-
-s2 = s2.map((word) => { return stem(word); });
-s1 = s1.map((word) => { return stem(word); });
-//console.log(s1);
-//console.log(s2);
+s1 = "CGTGAATTCAT";
+s2 = "GACTTAC";
 */
 
+var s1 = 'doctor told me that this was almost certainly a type of cancer that is incurable';
+var s2 = "About a year ago I was diagnosed with cancer. I had a scan at 7:30 in the morning, and it clearly showed a tumor on my pancreas. I didn’t even know what a pancreas was. The doctors told me this was almost certainly a type of cancer that is incurable, and that I should expect to live no longer than three to six months. My doctor advised me to go home and get my affairs in order, which is doctor’s code for prepare to die. It means to try to tell your kids everything you thought you’d have the next 10 years to tell them in just a few months. It means to make sure everything is buttoned up so that it will be as easy as possible for your family. It means to say your goodbyes. I lived with that diagnosis all day. Later that evening I had a biopsy, where they stuck an endoscope down my throat, through my stomach and into my intestines, put a needle into my pancreas and got a few cells from the tumor. I was sedated, but my wife, who was there, told me that when they viewed the cells under a microscope the doctors started crying because it turned out to be a very rare form of pancreatic cancer that is curable with surgery. I had the surgery and I’m fine now.";
+
+s1 = s1.split(' ');
+s2 = s2.toLowerCase().replace(/\./g, '').replace(/\,/g, '').split(' ');
+//console.log(s1);
+
+s2 = s2.map((word) => { return stem(word.trim()); });
+s1 = s1.map((word) => { return stem(word.trim()); });
+console.log(s1);
+console.log(s2);
+
+s2 = s2.filter(function(word) {
+    return stopWords.indexOf(word) == -1;
+})
+
+s1 = s1.filter(function(word) {
+    return stopWords.indexOf(word) == -1;
+});
+
+
+
+// Base componence (Cell)
 function Cell() {
   return {
       score: null,
@@ -39,7 +86,7 @@ var sCol = s2;
 var sMatch = 1;
 var sMismatch = -1;
 //var sGapStart = -1;
-var sGapCont = -1;
+var sGapCont = -2;
 
 var inDelChar = '-';
 
@@ -58,21 +105,46 @@ function formatNumberLength(num, length) {
 }
 
 function printTable(table, withArrow) {
-    for (var r = 0; r < table.length; r++) {
-        var cols = table[r];
+    console.log('*** Printed table ***');
+    /*
+    for (var c = 0; c < table.length; c++) {
+        var row = table[c];
         var rowString = '';
-        for (var c = 0; c < table.length; c++) {
-            rowString += formatNumberLength(cols[c].score, 5);
+        for (var r = 0; r < row.length; r++) {
+            rowString += formatNumberLength(row[r].score, 5);
 
             if (withArrow) {
-                rowString += ' [' + formatNumberLength(cols[c].arrows, 5) + ']';
+                rowString += ' [' + formatNumberLength(row[r].arrows, 5) + ']';
             }
 
-            if (c != table.length - 1)
+            if (r != table.length - 1)
                 rowString += ',';
         }
-        //console.log(rowString);
+        console.log(rowString);
     }
+    */
+
+    console.log(table.length);
+    console.log(table[0].length);
+
+    for (var r = 0; r < table[0].length; r++) {
+        var line = '';
+        for (var c = 0; c < table.length; c++) {
+
+            var cell = table[c][r];
+
+            line += formatNumberLength(cell.score, 5);
+            if (withArrow) {
+                line += ' [' + formatNumberLength(cell.arrows, 5) + ']';
+            }
+
+            if (c < table.length - 1) {
+                line += ',';
+            }
+        }
+        console.log(line);
+    }
+
 }
 
 // Initialize table
@@ -86,21 +158,19 @@ for (var c = 0; c < sCol.length + 1; c++) {
         var cell = new Cell();
         if (c === 0 && r === 0) {
             cell.score = 0;
-            table[c][r] = cell;
         } else if (r === 0) {
             cell.score = c * sGapCont;
-            table[c][r] = cell;
+            cell.arrows.push('L')
         } else if (c === 0) {
             cell.score = r * sGapCont;
-            table[c][r] = cell;
-        } else {
-            table[c][r] = cell;
+            cell.arrows.push('A');
         }
+        table[c][r] = cell
     }
 }
+console.log('Table was created: ' + table[0].length + ' X ' + table.length);
 
-//console.log(table);
-printTable(table);
+printTable(table, false);
 
 // Note that tring index is different from 1 from r and c
 for (var r = 1; r <= sRow.length; r++) {
@@ -141,47 +211,52 @@ printTable(table, true);
 var alignedRow = [];
 var alignedCol = [];
 
-for (var r = table.length - 1; r > 0; ) {
-    for (var c = table[0].length - 1; c > 0;) {
+for (var c = table.length - 1, r = table[0].length - 1; c > 0 || r > 0; ) {
+    var arrows = table[c][r].arrows;
+    //console.log(arrows);
 
-        var arrows = table[c][r].arrows;
-        //console.log(arrows);
+    switch (arrows[0]) { // TODO: Only supports the first path (diagonal path in priority)
+        case 'D':
+            alignedRow.push(sRow[r-1]); // PROBLEM!
+            alignedCol.push(sCol[c-1]);
+            r -= 1;
+            c -= 1;
+            break;
 
-        switch (arrows[0]) { // TODO: Only supports the first path (diagonal path in priority)
-            case 'D':
-                alignedRow.push(sRow[r-1]);
-                alignedCol.push(sCol[c-1]);
-                r -= 1;
-                c -= 1;
-                break;
+        case 'L':
+            alignedRow.push('-');
+            alignedCol.push(sCol[c-1]);
+            c -= 1;
+            break;
 
-            case 'L':
-                alignedRow.push('-');
-                alignedCol.push(sCol[c-1]);
-                c -= 1;
-                break;
+        case 'A':
+            //alignedRow.push(sRow[r-1]);
+            alignedCol.push('-');
+            r -= 1;
+            break;
 
-            case 'A':
-                alignedRow.push(sRow[r-1]);
-                alignedCol.push('-');
-                r -= 1;
-                break;
-
-            default:
-                console.error('Arrow error!');
-                break;
-        }
+        default:
+            console.log(c + " , " + r);
+            console.error('Arrow error!');
+            process.exit();
+            break;
 
     }
+
 }
 
 alignedRow.reverse();
 alignedCol.reverse();
 
-console.log(alignedRow);
-console.log(alignedCol);
+//console.log(alignedRow);
+//console.log(alignedCol);
 
+for (var i = 0; i < Math.max(alignedRow.length, alignedCol.length); i++) {
+    console.log(alignedRow[i] + ' \t ' + alignedCol[i]);
+}
+//console.log(alignedRow);
 
+return ;
 
 // TODO:
 // 1. 입력을 배열로 바꾸기
