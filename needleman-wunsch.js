19c4208e1244626916f568610716331ac874dfb8
JeungminOh <tangza@gmail.com>
2016-09-01 15:20:54 +0900

    Now algorithm supports array of string (sentence)

--- needleman-wunsch.js
+++ needleman-wunsch.js
@@ -3,11 +3,12 @@ var stem = require('stem-porter');
 var s1 = "GCATGCU";
 var s2 = "GATTACA";
 
-/*
+
 s1 = ['I', 'AM', 'HONORED', 'TO', 'BE', 'WITH', 'YOU', 'TODAY', 'AT', 'YOUR', 'GRADUATION'];
 s2 = ['I', 'AM', 'VERY', 'HAPPY', 'TO', 'STAY', 'WITH', 'ME', 'AT', 'YOUR', 'COMMENCEMENT'];
 // 순서가 중요하다!
 
+/*
 s2 = "About a year ago I was diagnosed with cancer. I had a scan at 7:30 in the morning, and it clearly showed a tumor on my pancreas. I didn’t even know what a pancreas was. The doctors told me this was almost certainly a type of cancer that is incurable, and that I should expect to live no longer than three to six months. My doctor advised me to go home and get my affairs in order, which is doctor’s code for prepare to die. It means to try to tell your kids everything you thought you’d have the next 10 years to tell them in just a few months. It means to make sure everything is buttoned up so that it will be as easy as possible for your family. It means to say your goodbyes. I lived with that diagnosis all day. Later that evening I had a biopsy, where they stuck an endoscope down my throat, through my stomach and into my intestines, put a needle into my pancreas and got a few cells from the tumor. I was sedated, but my wife, who was there, told me that when they viewed the cells under a microscope the doctors started crying because it turned out to be a very rare form of pancreatic cancer that is curable with surgery. I had the surgery and I’m fine now.";
 s2 = s2.toLowerCase().replace('.', '').replace(',', '').split(' ');
 //console.log(s1);
@@ -22,7 +23,7 @@ s1 = s1.map((word) => { return stem(word); });
 function Cell() {
   return {
       score: null,
-      arrow: []
+      arrows: []
   };
 }
 
@@ -40,12 +41,39 @@ var sMismatch = -1;
 //var sGapStart = -1;
 var sGapCont = -1;
 
-var blankChar = '-';
+var inDelChar = '-';
 
 /*
     s1 is at row, s2 is at column
 */
 var table = [];
+
+// Utilities
+function formatNumberLength(num, length) {
+    var r = "" + num;
+    while (r.length < length) {
+        r = " " + r;
+    }
+    return r;
+}
+
+function printTable(table, withArrow) {
+    for (var r = 0; r < table.length; r++) {
+        var cols = table[r];
+        var rowString = '';
+        for (var c = 0; c < table.length; c++) {
+            rowString += formatNumberLength(cols[c].score, 5);
+
+            if (withArrow) {
+                rowString += ' [' + formatNumberLength(cols[c].arrows, 5) + ']';
+            }
+
+            if (c != table.length - 1)
+                rowString += ',';
+        }
+        //console.log(rowString);
+    }
+}
 
 // Initialize table
 // r: row, c: col
@@ -77,54 +105,80 @@ printTable(table);
 // Note that tring index is different from 1 from r and c
 for (var r = 1; r <= sRow.length; r++) {
     for (var c = 1; c <= sCol.length; c++) {
+        var cell = table[c][r];
+
         //console.log(r + ":" + sRow[r-1] + ' , ' + c + ":" + sCol[c-1]);
         var scoreMatch = table[c-1][r-1].score + (sCol[c-1] == sRow[r-1] ? sMatch : sMismatch);
-        var scoreInsert = table[c-1][r].score + sGapCont;
-        var scoreDelete = table[c][r-1].score + sGapCont; // TODO: Insert and Delete might be changed
+        var scoreLeft = table[c-1][r].score + sGapCont;
+        var scoreAbove = table[c][r-1].score + sGapCont; // TODO: Insert and Delete might be changed
 
         // TODO: Implement the panelty for InDel continuted!
-        //console.log(scoreMatch + ' / ' + scoreDelete + ' / ' + scoreInsert + '\n');
-        table[c][r].score = Math.max(scoreMatch, scoreInsert, scoreDelete);
+        var updatedScore = Math.max(scoreMatch, scoreLeft, scoreAbove);
+        cell.score = updatedScore;
+
+        if (updatedScore == scoreMatch) {
+            cell.arrows.push('D'); // diagonal
+        }
+
+        if (updatedScore == scoreLeft) {
+            cell.arrows.push('L'); // left
+        }
+
+        if (updatedScore == scoreAbove) {
+            cell.arrows.push('A'); // above
+        }
+
         //console.log(table[c][r]);
     }
 }
 
-console.log('New');
 //console.log(table);
-printTable(table);
 
+console.log('New');
+printTable(table, true);
+
+// Track back the arrows to rebuild the aligned strings
 var alignedRow = '';
 var alignedCol = '';
 
-for (var r = alignedRow.length; r > 0; r--) {
-    for (var c = alignedCol.length; c >0; c--) {
+for (var r = table.length - 1; r > 0; ) {
+    for (var c = table[0].length - 1; c > 0;) {
 
-        var diagonal = table[c-1][r-1];
-        var above = table[c][r-1];
-        var left = table[c-1][r];
+        var arrows = table[c][r].arrows;
+        //console.log(arrows);
 
+        switch (arrows[0]) {
+            case 'D':
+                alignedRow = sRow[r-1] + ' ' + alignedRow;
+                alignedCol = sCol[c-1] + ' ' + alignedCol;
+                r -= 1;
+                c -= 1;
+                break;
 
-    }
-}
+            case 'L':
+                alignedRow = '-' + ' ' + alignedRow;
+                alignedCol = sCol[c-1] + ' ' + alignedCol;
+                c -= 1;
+                break;
 
-function formatNumberLength(num, length) {
-    var r = "" + num;
-    while (r.length < length) {
-        r = " " + r;
-    }
-    return r;
-}
+            case 'A':
+                alignedRow = sRow[r-1]+ ' ' + alignedRow;
+                alignedCol = '-'+ ' ' + alignedCol;
+                r -= 1;
+                break;
 
-function printTable(table) {
-    for (var r = 0; r < table.length; r++) {
-        var cols = table[r];
-        var colsString = '';
-        for (var c = 0; c < table.length; c++) {
-            colsString += formatNumberLength(cols[c].score, 5) + ',';
+            default:
+                console.error('Arrow error!');
+                break;
         }
-        console.log(colsString);
+
     }
 }
+
+console.log(alignedRow);
+console.log(alignedCol);
+
+
 
 // TODO:
 // 1. 입력을 배열로 바꾸기
